a0e69011897d19c2f2f4c96ae4d7896abf03146f
Tad-Min <183442531+Tad-Min@users.noreply.github.com>
2025-05-29 22:26:22 +0700

    upp

---
+++ PRN212_SE1852/callbyRederence/Program.cs
@@ -0,0 +1,15 @@
+﻿void doicho( int a, int b)
+{
+    int temp = a; a = b; b = temp;
+    Console.WriteLine("a = " + a);
+    Console.WriteLine("b = " + b);
+}
+
+int a = 19;
+int b = 8;
+ 
+Console.WriteLine("a = "+ a);
+Console.WriteLine("b = "+  b);
+
+doicho (a, b);
+
